7100ee8c0132378d7bd0287a749c507056061699
mary0427 <141801401+mary0427@users.noreply.github.com>
2023-08-09 16:58:43 +0900

    Add files via upload

---
+++ 1단계_입출력과 사칙연산/# 02_A+B.py
@@ -0,0 +1,19 @@
+# 02_A+B
+# Baekjoon_1000
+# 작성자 : 이지현
+
+result = 0
+s = input()
+l = s.split(' ')
+for i in l:
+    result += int(i)
+print(result)
+
+# split 함수 : 문자열을 잘라 배열로 변환
+'''문자열.split()
+
+문자열.split('구분자')
+
+문자열.split('구분자', 분할횟수)
+
+문자열.split(sep='구분자', maxsplit=분할횟수)'''
---
+++ 1단계_입출력과 사칙연산/# 03_A-B.py
@@ -0,0 +1,7 @@
+# 03_A-B
+# Baekjoon_1001
+# 작성자 : 이지현
+
+l = input().split()
+r = int(l[0])-int(l[1])
+print(r)
