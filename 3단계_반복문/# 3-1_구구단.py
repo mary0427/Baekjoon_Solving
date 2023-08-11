1563636587a7286aaf3b873a09618e866af7712a
mary0427 <141801401+mary0427@users.noreply.github.com>
2023-08-11 18:08:16 +0900

    Add files via upload

---
+++ 3단계_반복문/# 3-1_구구단.py
@@ -0,0 +1,18 @@
+# 3-1_구구단
+# Baekjoon_2739
+# 작성자 : 이지현
+
+n = int(input())
+for i in range(1,10):
+   print(f'{n} * {i} = {n*i}') 
+
+
+# format 함수
+'''
+d : 정수 (ex. 3,4,5,)
+S : 문자 (ex. 'python')
+f : 실수 (ex. 3.144545...)
+
+# 예시 : Print('%10s' % ('love'))
+https://funzozo.tistory.com/7 참고
+'''
